5a8d208b2f3849e8560ce5a949552630aa4f5171
kamrulhaider <kamrulhaider.ssb@gmail.com>
2021-09-20 00:46:11 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# towhid-class-1
---
+++ var.js
@@ -0,0 +1,71 @@
+var shoePrice = 10;
+shoePrice = 30;
+shoePrice = 50;
+shoePrice = 56;
+console.log(shoePrice)
+
+var name = 'Towhid';
+name = "";
+var number = 10;
+var isTrue = true;
+var nullVar = null;
+// console.log(isTrue)
+
+var typeCheck = typeof (name);
+// console.log(typeCheck)
+
+var a = 2;
+var b = 2;
+a++;
+a++;
+a++;
+console.log(a)
+var c = a + b;
+var d = a * b;
+var e = a / b;
+var f = a - b;
+
+var roshi = 100;
+var myPart = roshi % 3;
+// console.log(myPart)
+
+var myName = 'kamrul';
+var myFrnd = 'haider';
+
+// console.log(myName + ' ' + myFrnd)
+a = 20;
+var stringNumber = a + myFrnd;
+// console.log(stringNumber)
+
+a = 'Name';
+b = '12';
+// console.log(a - b);
+
+var convertNumber = parseInt(a);
+// console.log(convertNumber)
+
+var floatTest = '30.04457124875578';
+
+// console.log(parseFloat(floatTest))
+
+var floatingNum = parseFloat(floatTest);
+/* console.log(floatingNum);
+
+console.log(floatingNum.toFixed(2));
+
+ */
+
+var intTest = '12.64123456321';
+// console.log(parseInt(intTest));
+// console.log(parseFloat(intTest));
+
+var fixedFloat = parseFloat(intTest).toFixed(3);
+// console.log(parseFloat(fixedFloat));
+
+
+
+// myName name variable, setar man change kore console e dekhabi
+// prottek type er ekta kore variable nibi & sobar type of check kore console korbi
+// duita number variable nibi ebong (+-*/ and % ) kore console korbi
+// ekta dosomik songkha var nibi jeta string e thakbe and parseInt & parseFloat
+// parseFloat kora number ta dui dosomik sthan porjonto hobe and seta hobe number
